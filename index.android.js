ba669a93fa299c3b1d2d6ea75e7809e97eb72fba
Antero Fagerstedt <antero.fagerstedt@iki.fi>
2017-01-08 15:03:12 +0200

    added axios for backend integrations

--- index.android.js
+++ index.android.js
@@ -1,53 +1,77 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
+// import { WebView } from 'react-native';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  WebView,
 } from 'react-native';
 
+import axios from 'axios';
+
 export default class blazinglean extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+
+        /* TODO: check if we have auth token */
+
+        this.state = {
+            error: undefined,
+            auth_link: undefined,
+        };
+
+        // Make a request for a user with a given ID
+        axios.get('http://localhost:3000/authorizelink')
+        .then((response) => {
+            console.log(response);
+        })
+        .catch((error) => {
+            console.log(error);
+            this.setState({
+                error: error.message,
+            });
+        });
+    }
+
+    render() {
+        if (!this.state.auth_link) {
+            return (
+                <View>
+                    <Text>
+                        {this.state.error || 'Getting the authorization link...'}
+                    </Text>
+                </View>
+            );
+        }
+
+        return (
+            <WebView
+                source={{ uri: 'https://github.com/facebook/react-native' }}
+                style={{ marginTop: 20 }}
+            />
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
 
 AppRegistry.registerComponent('blazinglean', () => blazinglean);
